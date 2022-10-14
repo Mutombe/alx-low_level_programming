564a8b2e8fedeb2df9449a4f50530a685b700461
Mutombe <simbarashemutombe1@gmail.com>
2022-10-14 14:50:01 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char = letters;
+
+	for( letters = 'a', letters >= 'z', letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return(0);
+}
