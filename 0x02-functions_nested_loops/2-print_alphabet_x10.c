d6cd12985f379ca561a6dd40d5ceded099d044f4
Mutombe <simbarashemutombe1@gmail.com>
2022-10-19 05:03:24 -0700

    2-print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowecase and a newline
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+	while(i < 10)
+	{
+		ch = 'a';
+		while(ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
