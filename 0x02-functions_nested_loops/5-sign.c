001d6ef270bf4146d67d6661495a6f798d6f5b82
Mutombe <simbarashemutombe1@gmail.com>
2022-10-19 06:42:00 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-		printf("+");
 	}
 	if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
-		printf("-");
 	}
 	else 
 	{
-		return (0);
-		printf("0");
+		_putchar('0');
 	}
+	return (0);
 }
