36b8e5b8b663b0b96461013297798fe8f5b66eff
Mutombe <simbarashemutombe1@gmail.com>
2022-10-20 06:26:54 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul-Multiplies a and b.
+ * @a:Is the first variable.
+ * @b:Is the second variable.
+ * Return: Always return 0.
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	 mul =  a * b;
+
+	 return (mul);
+}
