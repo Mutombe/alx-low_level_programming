4b29ee85cf48663dd6d980c225d1dc0119e60619
Mutombe <simbarashemutombe1@gmail.com>
2022-10-20 05:53:24 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks if a character is upercase.
+ * @c:is the variable
+ * Return:Always return 0.
+ */
+
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _issuper(int c);
+
+
+
+
+
+#endif
