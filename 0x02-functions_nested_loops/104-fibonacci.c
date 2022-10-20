b42798af87e43a8b1d11afdc90d196d40849db38
Mutombe <simbarashemutombe1@gmail.com>
2022-10-20 03:29:50 -0700

    98 fibbo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/**main-Prints the first 98 Fibonacci numbers
+/**
+ * main-Prints the first 98 Fibonacci numbers
  *
  * Return:Always 0.
  */
 
 int main(void)
 {
-	int c, boolean,boolean2;
+	int c, boolean, boolean2;
 	long int n1, n2, fn, fn2, n11, n22;
 
 	n1 = 1;
@@ -16,16 +17,16 @@ int main(void)
 	printf("%ld %ld", n1, n2);
 	for (c = 0; c < 96; c++)
 	{
-		if(boolean)
+		if (boolean)
 		{
 		fn = n1 + n2;
-		printf(",%ld", fn);
+		printf(", %ld", fn);
 		n1 = n2;
 		n2 = fn;
 		}
 		else
 		{
-		if(boolean2)
+		if (boolean2)
 		{
 		n11 = n1 % 1000000000;
 		n22 = n2 % 1000000000;
@@ -35,7 +36,7 @@ int main(void)
 		}
 		fn2 = (n11 + n22);
 		fn = n1 + n2 + (fn2 / 1000000000);
-		printf(",%ld", fn);
+		printf(", %ld", fn);
 		printf("%ld", fn2 % 1000000000);
 		n1 = n2;
 		n11 = n22;
