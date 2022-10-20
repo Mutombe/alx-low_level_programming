a37641b06aa98a4e9096dc2cc901e20a225952f6
Mutombe <simbarashemutombe1@gmail.com>
2022-10-20 06:51:34 -0700

    except2and4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0-9 except 2 and 4
+ *
+ * Return:Always 0 .
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+	i = 0;
+
+	while (i > 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			i++;
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
