b39d001458f06273839388e834013b60f99de989
Mutombe <simbarashemutombe1@gmail.com>
2022-10-20 04:17:41 -0700

    times--tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,21 @@
  *
  * Return:no return
  */
+
 void times_table(void)
 {
 	int a, b, op;
+
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
+
 		for (b = 1; b <= 9; b++)
 		{
-			op = a * b ;
+			op = a * b;
 			_putchar(44);
 			_putchar(32);
+
 			if (op <= 9)
 			{
 			_putchar(32);
@@ -28,5 +32,6 @@ void times_table(void)
 			}
 		}
 		_putchar('\n');
+
 	}
 }
