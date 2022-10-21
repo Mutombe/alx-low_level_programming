230258a695b6a00965aacf6ebdc48e30d77f9ca0
Mutombe <simbarashemutombe1@gmail.com>
2022-10-21 10:23:49 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main-Prints Holberton as message
- *
+ * main - prints _putchar, followed by a newline
  * Returns: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-	
+	printf("_putchar");
+
 	return (0);
 }
