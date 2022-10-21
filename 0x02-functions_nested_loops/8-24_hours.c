08bb59ce0b4cc93b81bd53aa83d6fec388e51cce
Mutombe <simbarashemutombe1@gmail.com>
2022-10-21 06:54:06 -0700

    bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,37 +1,28 @@
-#include "main.h"                                                           
+#include "main.h"
 
-/**                                                                         
- *jack_bauer-print all minutes in a day from 00:00         
- *
- *Returns:0 if successful            
- */                                                                         
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer starting from 00:00 to 23:59
+ */
 
-                                                                            
-
-void jack_bauer(void)                                                       
+void jack_bauer(void)
 {
-	int i, j, k, m;
-       	for (i = 0; 1 <= 2; i++)
-       	{                                                     
-		 for (j = 0; j <= 9; j++)
-		 {                                                           
-	        	 for (k = 0; k <= 5; k++)
-			 {                                                   
-		         	for (m = 0; m <= 9; m++)
-				{
-					if (i == 2 && j > 3)
-					{
-					break;
-					}
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(m + '0');
-					_putchar('\n');
-				}
+	int i, j;
 
-			}
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
-}				
+}
