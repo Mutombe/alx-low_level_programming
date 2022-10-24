75d9c98146bdac2d864ce42f105c932f2f5932f9
Mutombe <simbarashemutombe1@gmail.com>
2022-10-24 14:25:13 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,13 @@
 
 /**
  * _strlen - gets the size of a string
- *
+ *@s: Variable
  * Return: The length of a string
  */
 int _strlen(char *s)
 {
 	int p = 0;
+
 	while (*(s + p) != 0)
 	{
 		p++;
