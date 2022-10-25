742cc2c745a40caff4530c80c01ba36681f6aec4
Mutombe <simbarashemutombe1@gmail.com>
2022-10-25 15:44:10 -0700

    copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0 ;
+	int l = 0;
 	int x = 0;
 
 	while (*(src + 1) != '\0')
 	{
 		l++;
 	}
-	for ( ; x < 1; x++)
+	for ( ; x < 1 ; x++)
 	{
 		dest[x] = src[x];
 	}
