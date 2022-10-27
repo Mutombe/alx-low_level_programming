d18f226c67e8d46a5f9ae0f6fda1d9f155d201a1
Mutombe <simbarashemutombe1@gmail.com>
2022-10-27 15:47:23 -0700

    alx projects

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != ' '; i++)
 		dest[i] = src[i];
-		for ( ; i < n; i++)
-			dest[i] = ' ';
+	for ( ; i < n; i++)
+		dest[i] = ' ';
 
-		return (dest);
+	return (dest);
 }
