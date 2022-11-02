69814a7c6f8564a9ac05c2341725c2c12843eb88
Mutombe <simbarashemutombe1@gmail.com>
2022-11-02 05:44:15 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *  _puts_recursion - return a string
- *
- *  Return: Always 0
+ * _puts_recursion - function like puts();
+ *  @s: input
+ *  Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	char str = *s;
-
-	while (str != '0')
-		printf("%c", str);
-	str++;
-}
-int main()
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts_recursion(char *s);
+extern void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,5 +10,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
+extern int _putchar(char c);
 #endif
