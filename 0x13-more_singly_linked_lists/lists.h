799d756ae3b8e1639a32d409fa07231a43901339
Mutombe <simbarashemutombe1@gmail.com>
2022-11-30 08:57:58 -0800

    linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,14 +9,13 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list node structure for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+}listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
